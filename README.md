5c33ef781d247a2994fd19ab073a2f1b1e135284
KotionLotion <2020151717@ub.edu.bz>
2026-09-25 10:07:02 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Extensions I used to make my life easier:
+https://marketplace.visualstudio.com/items?itemName=tonyjewel.d-code (hiughlights)
---
+++ test/test.d
@@ -0,0 +1,107 @@
+import std.stdio;
+import std.conv;
+import std.math;
+import std.random;
+import std.datetime;
+import std.algorithm;
+import std.array;
+import std.string;
+import std.format;
+import std.typecons : No;
+import std.typecons;
+import std.range : chain;
+import std.file;
+import std.parallelism;
+import std.concurrency;
+import std.range;
+
+/*
+Multiline comment
+*/
+void main(){
+    // write("What's your name ? ");
+    // string name;
+    // //we expect a string '%s' with a newline '\n' stored inside a vairable '&name'
+    // readf("%s\n", &name);
+    // writeln("Hello ", name);
+
+
+    // int a1 = 10;
+
+    // auto bool1 = true;
+    // writeln("Bool : ", bool1);
+    // writeln("bool min : ", bool.min);
+    // writeln("bool max : ", bool.max);
+    // bool happy = true;
+    // writefln("Bool for happy: %s", happy);
+
+    // \n \t \b \f \r \" \'
+
+
+    // //CASTING 
+    // int a = 15;
+    // short aShort = cast(short) a;
+    // short aShort2 = cast(short) 10000;
+
+    // writeln("aShort: ", aShort2);
+
+    // auto sInt = to!string(10);
+    // writeln("Type : ", typeof(sInt).stringof);
+
+    // auto iStr = to!int("10");
+    // writeln("Type : ", typeof(iStr).stringof);
+
+    // // INC MATH
+    // int incMe = 0;
+    // while(incMe<10){
+    //     writeln("Incrementing : ", incMe, " + 1");
+    //     incMe++;
+    // }
+
+    // int[10] a1;
+    // int [] a2 = [1, 2, 3, 4, 5];
+    // a2[0] = 10;
+
+    // writeln("Index : ", a2[0]);   
+
+    // int [] a3;
+
+    // a3 ~= 1;
+    // a3 ~= 2;
+    // a3 ~= 3;
+
+    // a3 = a3.remove(1);
+    // writeln(a3);
+
+    // a3 = a2 ~ a3;
+    // writeln(a3);
+
+    // a3 = a3.remove!(x => (x % 2 == 0));
+    // writeln(a3);
+
+    // writeln(sort(a3));
+    // writeln(reverse(a3));
+
+    // writeln(a3.replace(3,2));
+
+    // a3 = a2 ~ a3;
+    // writeln(a3[0 .. 3]);
+
+    // string [][][] a4 = [[["000"], ["100"], ["200"], ["300"]], 
+    //                     [["010"], ["110"], ["210"], ["310"]],
+    //                     [["020"], ["120"], ["220"], ["320"]]];
+    // writeln(a4[2][3][0]);
+
+    // string s1 = "Jesse";
+    // writeln(s1[0]);
+
+    // s1 = "Juan";
+    // writeln(s1.length);
+
+    // string s2 = s1 ~ "Thumb";
+    // writeln("T : ", indexOf(s2, 'T'));
+    // writeln("Last T : ", lastIndexOf(s2, 'T'));
+    // writeln("Thumb : ", indexOf(s2, "Thumb", No.caseSensitive));
+
+
+}
